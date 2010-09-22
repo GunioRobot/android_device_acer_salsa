4cd341903eb76113da673380ad61049530c0d1a9
xian1243 <sorgi.christian@gmail.com>
2010-09-22 19:56:52 +0200

    Fixed typo vendor -> device, re-added QSD8K keylayout files.

--- acer/liquid/liquid.mk
+++ acer/liquid/liquid.mk
@@ -5,8 +5,8 @@
 #     Created by Koudelka and xian1243                                      #
 #                                                                           #
 #############################################################################
-PRODUCT_COPY_FILES := \
-    device/acer/liquid/init.salsa.rc:root/init.salsa.rc
+#PRODUCT_COPY_FILES := \
+#    device/acer/liquid/init.salsa.rc:root/init.salsa.rc
 
 # Check generic.mk/languages_full.mk to see what applications/languages are installed turns out all languages get included if I don't specify, but some seem to be missing the actuall translation.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -97,54 +97,54 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/lib/libgps.so:obj/lib/libgps.so \
-vendor/acer/liquid/proprietary/lib/liboemcamera.so:obj/lib/liboemcamera.so
+device/acer/liquid/proprietary/lib/libgps.so:obj/lib/libgps.so \
+device/acer/liquid/proprietary/lib/liboemcamera.so:obj/lib/liboemcamera.so
 
 # All the blobs necessary for liquid
 # Copy EGL libraries
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/lib/libril-acer-1.so:system/lib/libril-acer-1.so \
-vendor/acer/liquid/proprietary/lib/libril-acerril-hook-oem.so:system/lib/libril-acerril-hook-oem.so
+device/acer/liquid/proprietary/lib/libril-acer-1.so:system/lib/libril-acer-1.so \
+device/acer/liquid/proprietary/lib/libril-acerril-hook-oem.so:system/lib/libril-acerril-hook-oem.so
 
 # Copy EGL libraries
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/lib/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-vendor/acer/liquid/proprietary/lib/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-vendor/acer/liquid/proprietary/lib/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-vendor/acer/liquid/proprietary/lib/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-vendor/acer/liquid/proprietary/lib/libgsl.so:system/lib/libgsl.so
+device/acer/liquid/proprietary/lib/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+device/acer/liquid/proprietary/lib/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+device/acer/liquid/proprietary/lib/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+device/acer/liquid/proprietary/lib/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+device/acer/liquid/proprietary/lib/libgsl.so:system/lib/libgsl.so
 
 #Copy camera libraries
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/lib/libcamera.so:system/lib/libcamera.so \
-vendor/acer/liquid/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
-vendor/acer/liquid/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
-vendor/acer/liquid/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so
+device/acer/liquid/proprietary/lib/libcamera.so:system/lib/libcamera.so \
+device/acer/liquid/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
+device/acer/liquid/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
+device/acer/liquid/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so
 
 # Copy dhcpd (need to chmod dhcpcd-eth0.pid upon flashing)
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
-vendor/acer/liquid/proprietary/etc/dhcpcd/dhcpcd-run-hooks:system/etc/dhcpcd/dhcpcd-run-hooks \
-vendor/acer/liquid/proprietary/etc/dhcpcd/dhcpcd-hooks/01-test:system/etc/dhcpcd/dhcpcd-hooks/01-test \
-vendor/acer/liquid/proprietary/etc/dhcpcd/dhcpcd-hooks/20-dns.conf:system/etc/dhcpcd/dhcpcd-hooks/20-dns.conf \
-vendor/acer/liquid/proprietary/etc/dhcpcd/dhcpcd-hooks/95-configured:system/etc/dhcpcd/dhcpcd-hooks/95-configured \
-vendor/acer/liquid/proprietary/data/misc/dhcp/dhcpcd-eth0.pid:data/misc/dhcp/dhcpcd-eth0.pid
+device/acer/liquid/proprietary/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+device/acer/liquid/proprietary/etc/dhcpcd/dhcpcd-run-hooks:system/etc/dhcpcd/dhcpcd-run-hooks \
+device/acer/liquid/proprietary/etc/dhcpcd/dhcpcd-hooks/01-test:system/etc/dhcpcd/dhcpcd-hooks/01-test \
+device/acer/liquid/proprietary/etc/dhcpcd/dhcpcd-hooks/20-dns.conf:system/etc/dhcpcd/dhcpcd-hooks/20-dns.conf \
+device/acer/liquid/proprietary/etc/dhcpcd/dhcpcd-hooks/95-configured:system/etc/dhcpcd/dhcpcd-hooks/95-configured \
+device/acer/liquid/proprietary/data/misc/dhcp/dhcpcd-eth0.pid:data/misc/dhcp/dhcpcd-eth0.pid
 
 # Copy GPS libraries and dependencies
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/lib/ibloc.so:system/lib/libloc.so \
-vendor/acer/liquid/proprietary/lib/libloc-rpc.so:system/lib/libloc-rpc.so \
-vendor/acer/liquid/proprietary/lib/libloc_api.so:system/lib/libloc_api.so \
-vendor/acer/liquid/proprietary/lib/libcommondefs.so:system/lib/libcommondefs.so \
-vendor/acer/liquid/proprietary/lib/libgps.so:system/lib/libgps.so
+device/acer/liquid/proprietary/lib/ibloc.so:system/lib/libloc.so \
+device/acer/liquid/proprietary/lib/libloc-rpc.so:system/lib/libloc-rpc.so \
+device/acer/liquid/proprietary/lib/libloc_api.so:system/lib/libloc_api.so \
+device/acer/liquid/proprietary/lib/libcommondefs.so:system/lib/libcommondefs.so \
+device/acer/liquid/proprietary/lib/libgps.so:system/lib/libgps.so
 
 # Copy WiFi firmware and config
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/etc/firmware/BCM4325.hcd:system/etc/firmware/BCM4325.hcd \
-vendor/acer/liquid/proprietary/etc/firmware/BCM4325.bin:system/etc/firmware/BCM4325.bin \
-vendor/acer/liquid/proprietary/etc/firmware/BCM4325_apsta.bin:system/etc/firmware/BCM4325_apsta.bin \
-vendor/acer/liquid/proprietary/etc/wifi/nvram.txt:system/etc/wifi/nvram.txt \
-vendor/acer/liquid/proprietary/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+device/acer/liquid/proprietary/etc/firmware/BCM4325.hcd:system/etc/firmware/BCM4325.hcd \
+device/acer/liquid/proprietary/etc/firmware/BCM4325.bin:system/etc/firmware/BCM4325.bin \
+device/acer/liquid/proprietary/etc/firmware/BCM4325_apsta.bin:system/etc/firmware/BCM4325_apsta.bin \
+device/acer/liquid/proprietary/etc/wifi/nvram.txt:system/etc/wifi/nvram.txt \
+device/acer/liquid/proprietary/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
 # Copy Vold and configs
 PRODUCT_COPY_FILES += \
@@ -154,20 +154,20 @@ device/acer/liquid/proprietary/bin/vold:system/bin/vold
 
 # Copy sensor library, binary and configuration (need to chmod ms3c_yamaha.cfg upon flashing for yamaha sensor to function properly)
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-vendor/acer/liquid/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-vendor/acer/liquid/proprietary/lib/sensors.salsa.so:system/lib/hw/sensors.salsa.so \
-vendor/acer/liquid/proprietary/lib/libsensor_yamaha.so:system/lib/libsensor_yamaha.so \
-vendor/acer/liquid/proprietary/lib/libms3c_yamaha.so:system/lib/libms3c_yamaha.so \
-vendor/acer/liquid/proprietary/bin/sensorcalibutil_yamaha:system/bin/sensorcalibutil_yamaha \
-vendor/acer/liquid/proprietary/bin/sensorserver_yamaha:system/bin/sensorserver_yamaha \
-vendor/acer/liquid/proprietary/bin/sensorstatutil_yamaha:system/bin/sensorstatutil_yamaha \
-vendor/acer/liquid/proprietary/data/system/ms3c_yamaha.cfg:data/system/ms3c_yamaha.cfg
+device/acer/liquid/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+device/acer/liquid/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+device/acer/liquid/proprietary/lib/sensors.salsa.so:system/lib/hw/sensors.salsa.so \
+device/acer/liquid/proprietary/lib/libsensor_yamaha.so:system/lib/libsensor_yamaha.so \
+device/acer/liquid/proprietary/lib/libms3c_yamaha.so:system/lib/libms3c_yamaha.so \
+device/acer/liquid/proprietary/bin/sensorcalibutil_yamaha:system/bin/sensorcalibutil_yamaha \
+device/acer/liquid/proprietary/bin/sensorserver_yamaha:system/bin/sensorserver_yamaha \
+device/acer/liquid/proprietary/bin/sensorstatutil_yamaha:system/bin/sensorstatutil_yamaha \
+device/acer/liquid/proprietary/data/system/ms3c_yamaha.cfg:data/system/ms3c_yamaha.cfg
 
 # Copy Acer keylayout and headset driver
 PRODUCT_COPY_FILES += \
-vendor/acer/liquid/proprietary/usr/keylayout/a1-keypad.kl:system/usr/keylayout/a1-keypad.kl \
-vendor/acer/liquid/proprietary/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
-vendor/acer/liquid/proprietary/usr/keylayout/acer-hs-butt.kl:system/usr/keylayout/acer-hs-butt.kl \
-vendor/acer/liquid/proprietary/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-vendor/acer/liquid/proprietary/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
+device/acer/liquid/proprietary/usr/keylayout/a1-keypad.kl:system/usr/keylayout/a1-keypad.kl \
+device/acer/liquid/proprietary/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+device/acer/liquid/proprietary/usr/keylayout/acer-hs-butt.kl:system/usr/keylayout/acer-hs-butt.kl \
+device/acer/liquid/proprietary/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+device/acer/liquid/proprietary/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl
