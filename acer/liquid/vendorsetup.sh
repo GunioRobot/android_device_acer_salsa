31bb11a75b3bec8e276cad2e31cadec04e822e54
xian1243 <sorgi.christian@gmail.com>
2010-09-23 18:38:15 +0200

    Not committing any code, but rather correcting my last commit's message. -> Fixed vendorsetup.sh for CyanogenMod build (added cyanogen_liquid to lunch_combo).

--- acer/liquid/vendorsetup.sh
+++ acer/liquid/vendorsetup.sh
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 # This file is executed by build/envsetup.sh, and can use anything
 # defined in envsetup.sh.
 #
