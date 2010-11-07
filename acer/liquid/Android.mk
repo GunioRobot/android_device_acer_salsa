634fe87a73cc7fcef7ed9a9d41e9e75141d52b30
xian1243 <sorgi.christian@gmail.com>
2010-11-07 19:29:06 +0100

    Added libcamera based on CA, RE, and trial and error (thanks to rmcc).

--- acer/liquid/Android.mk
+++ acer/liquid/Android.mk
@@ -14,6 +14,7 @@
 
 LOCAL_PATH := $(my-dir)
 subdir_makefiles := \
-	$(LOCAL_PATH)/liblights/Android.mk
+	$(LOCAL_PATH)/liblights/Android.mk \
+	$(LOCAL_PATH)/libcamera/Android.mk
 
 include $(subdir_makefiles)
